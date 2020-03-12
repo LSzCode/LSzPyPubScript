cfdfc38acc08ca4a5311e9d42d677b345c8ef9ae
LSzCode <57759709+LSzCode@users.noreply.github.com>
2020-03-12 12:34:15 +0100

    Add files via upload

---
+++ TEST_b2.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1) Weise das erste und letzt Element von *t* jeweils neu zu. Prüfe beide Objekte auf Gleichheit.
+
+# In[1]:
+
+
+t = "drill"
+
+
+# In[ ]:
+
+
+
+
+
+# 2) Wieviele Ebenen hat diese Liste? Von was für einer Art von Liste spricht man hier im Englischen?
+
+# In[3]:
+
+
+L = [[[1,2],[3,4]],5]
+
+
+# In[ ]:
+
+
+
+
+
+# 3) Ändere *w1*, *w2*, *w3* und *w4* so, dass *world* mit einem Großbuchstaben beginnt. Nutze dafür die Funktion *replace()*.
+
+# In[5]:
+
+
+w1 =  'Hello world'
+w2 =  ['Hello', 'world']
+w3 = ['Hello, world']
+w4 = 'Hello wide world'
+
+
+# In[ ]:
+
+
+
+
+
+# 4) Ändere *w1*, *w2*, *w3* und *w4* so, dass *world* mit einem Großbuchstaben beginnt. Nutze dafür die Funktion *title()*.
+
+# In[10]:
+
+
+w1 = 'Hello world'
+w2 = ['Hello', 'world']
+w3 = ['Hello, world']
+w4 = ['Hello', 'wide', 'world']
+
+
+# In[ ]:
+
+
+
+
+
+# 5) Gebe die Liste *order* so aus, dass die Elemente als *python* zu lesen sind. Nutze dazu Indizierung.
+
+# In[45]:
+
+
+order1 = ['p','x','y','x','t','x','h','x','o','x','n']
+order2 = ['x','p','x','y','x','t','x','h','x','o','x','n']
+order3 = ['n','x','o','x','h','x','t','x','y','x','p']
+#ergebnis = ['p','y','t','h','o','n']
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
